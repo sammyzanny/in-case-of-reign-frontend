46cb866b390e7281c20074407d5c95d82de1e734
github username <github email address>
2020-03-02 16:44:28 -0600

    Form

---
+++ src/index.js
@@ -0,0 +1,32 @@
+let CURRENT_USER;
+const LOG_IN_FORM = document.querySelector(".create-user-form")
+
+function main(){
+    addFormListener();
+}
+
+
+function addFormListener(){
+    LOG_IN_FORM.addEventListener("submit", event => {
+        event.preventDefault();
+        const name = event.target[0].value;
+        const title = event.target[1].value
+
+        const reqObj = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+               name,
+               title 
+            })
+        }
+
+        fetch("http://localhost:3000/users", reqObj)
+        .then(resp => resp.json())
+        .then(user => console.log(user))
+    })
+}
+
+main();
